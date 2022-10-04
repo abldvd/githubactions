fbb69a7e1b127633c1839ec846c56ef01d67ca40
abaresag <abldvd.aresag@gmail.com>
2022-10-04 19:49:36 +0000

    Añadidos mas doctests

--- modeltools/preprocessing.py
+++ modeltools/preprocessing.py
@@ -20,5 +20,27 @@ def get_numerical_features(df):
     >>> df = pd.DataFrame({"a":[1]})
     >>> get_numerical_features(df)
     ['a']
+    
+    >>> df = pd.DataFrame([[1,"a"]])
+    >>> get_numerical_features(df)
+    [0]
+
+    >>> df = pd.DataFrame({"a": [" "]})
+    >>> get_numerical_features(df)
+    []
+
+    >>> get_numerical_features(pd.DataFrame())
+    []
+
+    >>> df = pd.DataFrame({"a": pd.Series(dtype=int)})
+    >>> get_numerical_features(df)
+    ['a']
+
+    >>> df = pd.DataFrame({"int": [1],"float": [1.]})
+    >>> get_numerical_features(df)
+    ['int', 'float']
+
+
+
     """
     return list(df.select_dtypes(include=[np.number]).columns)
